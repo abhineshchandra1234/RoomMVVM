838390d56b518a1c292f05e39d5291be22868f5d
abhineshchandra1234 <52635220+abhineshchandra1234@users.noreply.github.com>
2021-03-28 16:59:49 +0530

    added viewbinding app is working fine

--- app/src/main/java/com/example/roommvvm/MainActivity.kt
+++ app/src/main/java/com/example/roommvvm/MainActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.roommvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
         setupActionBarWithNavController(findNavController(R.id.navHostFragment))
