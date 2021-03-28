b0c1261734b9fea72b54aa657daad804dadcd42f
abhineshchandra1234 <52635220+abhineshchandra1234@users.noreply.github.com>
2021-03-28 19:54:21 +0530

    add fab onclick

--- app/src/main/java/com/example/roommvvm/Fragments/NotesList.kt
+++ app/src/main/java/com/example/roommvvm/Fragments/NotesList.kt
@@ -5,11 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.roommvvm.R
+import com.example.roommvvm.databinding.FragmentNotesListBinding
 
 
 class NotesList : Fragment(R.layout.fragment_notes_list) {
+    private lateinit var binding: FragmentNotesListBinding
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+            binding = FragmentNotesListBinding.bind(view)
+
+
+
+        fabOnClick()
+    }
+
+    private fun fabOnClick() {
+        binding.fabAddNotes.setOnClickListener {
+            findNavController().navigate(R.id.action_notesList_to_notesCreation)
+        }
     }
 }
