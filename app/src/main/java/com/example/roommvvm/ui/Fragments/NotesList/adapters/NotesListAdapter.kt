963f626c5ff11b41e4e52918b6ff421ca2701d97
abhineshchandra1234 <52635220+abhineshchandra1234@users.noreply.github.com>
2021-03-29 20:56:53 +0530

    added adapter class

--- app/src/main/java/com/example/roommvvm/ui/Fragments/NotesList/adapters/NotesListAdapter.kt
+++ app/src/main/java/com/example/roommvvm/ui/Fragments/NotesList/adapters/NotesListAdapter.kt
@@ -1,9 +1,16 @@
 package com.example.roommvvm.ui.Fragments.NotesList.adapters
 
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.roommvvm.R
 import com.example.roommvvm.data.models.Note
+import com.example.roommvvm.ui.Fragments.NotesList.NotesList
+import com.example.roommvvm.ui.Fragments.NotesList.NotesListDirections
+import kotlinx.android.synthetic.main.recycler_note_item.view.*
 
 class NotesListAdapter: RecyclerView.Adapter<NotesListAdapter.MyViewHolder>() {
 
@@ -13,18 +20,25 @@ class NotesListAdapter: RecyclerView.Adapter<NotesListAdapter.MyViewHolder>() {
     inner class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         init {
-            TODO()
+            itemView.cvNotes.setOnClickListener {
+                val position = adapterPosition
+                Log.d(TAG, "Title: ${noteList[position].note_title}")
+
+                val action = NotesListDirections.actionNotesListToNotesUpdation(noteList[position])
+                itemView.findNavController().navigate(action)
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.recycler_note_item,parent,false))
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentNote = noteList[position]
 
-        TODO()
+        holder.itemView.tvTitle.text = currentNote.note_title
+        holder.itemView.tvDescription.text = currentNote.note_description
     }
 
     override fun getItemCount(): Int {
